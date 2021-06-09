8b020bb9073732a46e7b727e087ed238bb1fed44
Kevin Henry <kevinhenry@mac.com>
2021-06-09 00:18:34 -0700

    EOD 8JUN21

---
+++ math_series/series.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    """
+    Function has one par
+    return n
+
+def lucus(n):
+    return n
---
+++ tests/test_series.py
@@ -0,0 +1,5 @@
+from math_series.series import fibonacci, lucas
+
+def test_fibonacci():
+    actual = fibonacci(1)
+    
