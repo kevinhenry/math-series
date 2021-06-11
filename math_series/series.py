99e4c7a31364c85b90e51835f053612479347a24
Kevin Henry <kevinhenry@mac.com>
2021-06-10 23:59:02 -0700

    Tests Passed and Readme Updated

--- math_series/series.py
+++ math_series/series.py
@@ -1,6 +1,6 @@
 def fibonacci(n):
     """
-    Fibonacci through iteration
+    Fibonacci testing with one parameter n, returning the nth value int he fibonacci series. Implemented through iteration.
     """
     seq_list = [0, 1]
     for i in range(2, n + 1):
@@ -11,7 +11,7 @@ def fibonacci(n):
 
 def lucas(n):
     """
-    Lucas testing - iteration
+    Lucas testing that returns the nth value in lucas numbers through iteration
     """
 
     seq_list = [2, 1]
@@ -23,7 +23,7 @@ def lucas(n):
 
 def sum_series(n, first=0, second=1):
     """
-    Sum Series testing
+    Sum Series testing with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional paraments will have default values of 0 and 1, and will determine the first two values for the series to be produced.
     """
 
     seq_list = [first, second]
