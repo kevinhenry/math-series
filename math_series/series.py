e83c0b23a5542e86254c5dff937bdbb820fad1f7
Kevin Henry <kevinhenry@mac.com>
2021-06-10 21:47:37 -0700

    8 Tests Passed

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,33 @@
 def fibonacci(n):
     """
-    Function has one par
-    return n
+    Fibonacci through iteration
+    """
+    seq_list = [0, 1]
+    for i in range(2, n + 1):
+        next_n = seq_list[-1] + seq_list[-2]
+        seq_list.append(next_n)
+    return seq_list[n - 1]
 
-def lucus(n):
-    return n
+
+def lucas(n):
+    """
+    Lucas testing - iteration
+    """
+
+    seq_list = [2, 1]
+    for i in range(2, n + 1):
+        next_n = seq_list[-1] + seq_list[-2]
+        seq_list.append(next_n)
+    return seq_list[n - 1]
+
+
+def sum_series(n, first=0, second=1):
+    """
+    Sum Series testing
+    """
+
+    seq_list = [first, second]
+    for i in range(2, n + 1):
+        next_n = seq_list[-1] + seq_list[-2]
+        seq_list.append(next_n)
+    return seq_list[n - 1]
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,42 @@
-from math_series.series import fibonacci, lucas
+from math_series import __version__
+from math_series.series import fibonacci, lucas, sum_series
 
-def test_fibonacci():
-    actual = fibonacci(1)
-    
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_one():
+    actual = fibonacci(4)
+    expected = 2
+    assert actual == expected
+
+
+def test_two():
+    actual = fibonacci(8)
+    expected = 13
+    assert actual == expected
+
+
+def test_three():
+    actual = lucas(4)
+    expected = 4
+    assert actual == expected
+
+
+def test_four():
+    actual = lucas(8)
+    expected = 29
+    assert actual == expected
+
+
+def test_five():
+    actual = sum_series(4)
+    expected = 2
+    assert actual == expected
+
+
+def test_six():
+    actual = sum_series(8)
+    expected = 13
+    assert actual == expected
